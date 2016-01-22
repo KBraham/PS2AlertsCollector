c2d52e6f4b629fe0665feabba5f73afb76851fa4
Koen Braham <koenbraham@gmail.com>
2016-01-22 15:46:42 +0100

    New structure for the routers

--- bin/main.js
+++ bin/main.js
@@ -2,27 +2,42 @@
 
 console.log("Starting collector");
 
-let MessageParser = require('../lib/message_parser.js');
-let CombatParser = require('../lib/combat_parser.js');
-
+let routerPush = require('../lib/router_push.js');
+let rounterInt = require('../lib/router_int.js');
 
 console.log("Importing modules [done]");
 
 
-let ML = new MessageParser();
-
-ML.onmessage("aaah, oky!");
+// let cp = new CombatParser();
 
 
 
-let cp = new CombatParser();
+let messages = [
+    {"payload":{"duration_held":"7679","event_name":"FacilityControl","facility_id":"293000","new_faction_id":"3","old_faction_id":"3","timestamp":"1405291671","world_id":"10","zone_id":"4"},"service":"event","type":"serviceMessage"},
+    {"payload":{"duration_held":"108","event_name":"FacilityControl","facility_id":"310088","new_faction_id":"1","old_faction_id":"3","timestamp":"1405291709","world_id":"10","zone_id":"282198111"},"service":"event","type":"serviceMessage"},
+    {"payload":{"duration_held":"18249","event_name":"FacilityControl","facility_id":"264000","new_faction_id":"1","old_faction_id":"1","timestamp":"1405291728","world_id":"10","zone_id":"4"},"service":"event","type":"serviceMessage"},
+    {"payload":{"duration_held":"122","event_name":"FacilityControl","facility_id":"310088","new_faction_id":"1","old_faction_id":"3","timestamp":"1405291733","world_id":"10","zone_id":"282263647"},"service":"event","type":"serviceMessage"},
+    {"payload":{"duration_held":"1405291744","event_name":"FacilityControl","facility_id":"310612","new_faction_id":"1","old_faction_id":"0","timestamp":"1405291744","world_id":"10","zone_id":"282329183"},"service":"event","type":"serviceMessage"},
+    {"payload":{"duration_held":"0","event_name":"FacilityControl","facility_id":"310612","new_faction_id":"3","old_faction_id":"1","timestamp":"1405291744","world_id":"10","zone_id":"282329183"},"service":"event","type":"serviceMessage"},
+    {"payload":{"duration_held":"1405291744","event_name":"FacilityControl","facility_id":"310088","new_faction_id":"3","old_faction_id":"0","timestamp":"1405291744","world_id":"10","zone_id":"282329183"},"service":"event","type":"serviceMessage"},
+    {"payload":{"duration_held":"2210","event_name":"FacilityControl","facility_id":"222380","new_faction_id":"3","old_faction_id":"3","timestamp":"1405291761","world_id":"10","zone_id":"6"},"service":"event","type":"serviceMessage"}
+];
 
-let receivers = [ML, cp];
-cp.onmessage("aah, this is convenient!");
 
-for(let x of receivers){
-  x.onmessage("message");
+
+for(let i = 0; i < messages.length; ++i) {
+    routerPush.onmessage(messages[i]);
 }
 
 
-console.log(`Number of processed messages: ${cp.number_messages}`);
+var lineReader = require('readline').createInterface({
+  input: require('fs').createReadStream('input1.txt')
+});
+
+lineReader.on('line', function (line) {
+    routerPush.onrawmessage(line);
+});
+
+lineReader.on('close', function(){
+    console.log(`Number of service messages: ${routerPush.number_service_messages()} / ${routerPush.number_messages()}`);    
+});
